dde34c9ad9f3baa3a197d747de0d2e91372de875
BorisPanich <panich2303@gmail.com>
2021-09-16 06:40:54 +0300

    Updates

---
+++ precache-manifest.375e9a5936e8cddbbfb5c6f1fc4335a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae400783a14ff7347f92f1d81871265",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "24631d53fefc71354203",
+    "url": "/TodoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "f8edfdd30cfe1adf67cc",
+    "url": "/TodoList/static/js/2.07ede5bd.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodoList/static/js/2.07ede5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24631d53fefc71354203",
+    "url": "/TodoList/static/js/main.a7298f91.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
